816b76456eca63d3e4bc82cfd1147d76b7d4d03a
Tristan Stérin <tristan.sterin@mu.ie>
2022-01-29 11:20:03 +0000

    ok

--- argument-cyclers/go.mod
+++ argument-cyclers/go.mod
@@ -3,7 +3,5 @@ module bbchallenge-arguments/argument-cyclers
 go 1.16
 
 require (
-	github.com/bbchallenge/bbchallenge-go v0.0.0-20220128180312-6e1818969140
-	github.com/rgeoghegan/tabulate v0.0.0-20220120200247-b9fe66735853
-	golang.org/x/exp v0.0.0-20220126164734-073fb1339172 // indirect
+	github.com/bbchallenge/bbchallenge-go v0.0.0-20220128181024-c8764e8703dd
 )
--- argument-cyclers/main.go
+++ argument-cyclers/main.go
@@ -160,8 +160,8 @@ func main() {
 
 	argTimeLimit := flag.Int("t", 1000, "time limit")
 	argSpaceLimit := flag.Int("s", 500, "space limit")
-	argMinIndex := flag.Int("m", 0, "min machine index in seed database")
-	argMaxIndex := flag.Int("m", bbc.TOTAL_UNDECIDED_TIME, "min machine index in seed database")
+	argMinIndex := flag.Int("m", 0, "min machine index to consider in seed database")
+	argMaxIndex := flag.Int("M", bbc.TOTAL_UNDECIDED_TIME, "max machine index to consider in seed database")
 	argNWorkers := flag.Int("n", 1000, "workers")
 
 	flag.Parse()
