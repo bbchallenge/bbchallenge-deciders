e74a48c8a1bb3cc4c0140d0dd58235a2e3989cba
Tristan Stérin <tristan.sterin@mu.ie>
2022-01-29 18:26:58 +0000

    Translated cyclers ready to rock

--- argument-translated-cyclers/main.go
+++ argument-translated-cyclers/main.go
@@ -1,15 +1,158 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sync"
+	"time"
 
 	bbc "github.com/bbchallenge/bbchallenge-go"
 )
 
 const DB_PATH = "../all_5_states_undecided_machines_with_global_header"
+const MAX_STATES = 5
+
+type TapePosition struct {
+	Symbol       byte
+	LastTimeSeen int
+	Seen         bool
+}
+
+type Record struct {
+	Tape     []TapePosition
+	Time     int
+	Position int
+}
+
+func recordsAreEquivalent(minSide bool, pastRecord *Record, currRecord *Record) bool {
+	offset := 0
+
+	//fmt.Println("\t", tapeRepr(pastRecord.Tape, pastRecord.Position, 10))
+	//fmt.Println("\t", tapeRepr(currRecord.Tape, currRecord.Position, 10))
+
+	for {
+
+		//fmt.Println(offset)
+
+		if pastRecord.Position+offset < 0 || pastRecord.Position+offset >= len(currRecord.Tape) {
+			break
+		}
+
+		if !currRecord.Tape[pastRecord.Position+offset].Seen || currRecord.Tape[pastRecord.Position+offset].LastTimeSeen < pastRecord.Time {
+			break
+		}
+
+		if currRecord.Tape[currRecord.Position+offset].Symbol != pastRecord.Tape[pastRecord.Position+offset].Symbol {
+			return false
+		}
+
+		if !minSide {
+			offset -= 1
+		} else {
+			offset += 1
+		}
+	}
+
+	return true
+}
+
+func tapeRepr(tape []TapePosition, pos int, radius int) (toRet string) {
+	origin := len(tape) / 2
+	for i := -1 * radius / 2; i < radius/2; i += 1 {
+		if origin+i == pos {
+			toRet += "->"
+		}
+		if tape[origin+i].Symbol == 0 {
+			toRet += "0"
+		} else {
+			toRet += "1"
+		}
+
+	}
+	return toRet
+}
+
+func argumentTranslatedCyclers(tm bbc.TM, timeLimit int, spaceLimit int) bool {
+
+	tapeMemory := 2 * spaceLimit
+
+	var tape []TapePosition = make([]TapePosition, tapeMemory)
+
+	currPos := tapeMemory / 2
+	nextPos := currPos
+	write := byte(0)
+	currState := byte(1)
+	currTime := 0
+
+	// [tapeSide][state][symbol] -> tapes
+	var recordHolders map[bool]map[byte]map[byte][]Record = make(map[bool]map[byte]map[byte][]Record)
+
+	minPosSeen := tapeMemory / 2
+	maxPosSeen := tapeMemory / 2
+
+	var err error
+
+	for err == nil && currState > 0 && currState <= MAX_STATES {
+
+		if currTime > timeLimit {
+			return false
+		}
+
+		read := tape[currPos].Symbol
+		//fmt.Println(currTime, currState, read, tapeRepr(tape, currPos, 10))
+		// We are breaking a record
+		if currPos < minPosSeen || currPos > maxPosSeen {
+			//fmt.Println("^")
+			var record Record
+			record.Tape = make([]TapePosition, len(tape))
+			copy(record.Tape, tape)
+			record.Time = currTime
+			record.Position = currPos
+
+			minSide := currPos < minPosSeen
+			if _, ok := recordHolders[minSide]; !ok {
+				recordHolders[minSide] = make(map[byte]map[byte][]Record)
+			}
+			if _, ok := recordHolders[minSide][currState]; !ok {
+				recordHolders[minSide][currState] = make(map[byte][]Record)
+			}
+
+			// The record has been broken in the past
+			if _, ok := recordHolders[minSide][currState][read]; ok {
+				for _, pastRecord := range recordHolders[minSide][currState][read] {
+					if recordsAreEquivalent(minSide, &pastRecord, &record) {
+						return true
+					}
+				}
+			}
+
+			recordHolders[minSide][currState][read] = append(recordHolders[minSide][currState][read], record)
+
+			minPosSeen = bbc.MinI(minPosSeen, currPos)
+			maxPosSeen = bbc.MaxI(maxPosSeen, currPos)
+		}
+
+		if maxPosSeen-minPosSeen > spaceLimit || currPos < 0 || currPos >= len(tape) {
+			return false
+		}
+
+		tape[currPos].Seen = true
+		tape[currPos].LastTimeSeen = currTime
+
+		write, currState, nextPos = bbc.TmStep(tm, read, currState, currPos, currTime)
+
+		tape[currPos].Symbol = write
+
+		currPos = nextPos
+		currTime += 1
+
+	}
+
+	return false
+}
 
 func main() {
 
@@ -43,6 +186,44 @@ func main() {
 	spaceLimit := *argSpaceLimit
 	nWorkers := *argNWorkers
 
-	fmt.Println(minIndex, maxIndex, timeLimit, spaceLimit, nWorkers)
+	// fmt.Println(minIndex, maxIndex, timeLimit, spaceLimit, nWorkers)
 
+	// n := 7888060 // not a translated cycler
+	// m, _ := bbc.GetMachineI(DB[:], n, true)
+
+	// fmt.Println(argumentTranslatedCyclers(m, timeLimit, spaceLimit))
+
+	runName := "output/" + bbc.GetRunName() + "tranlated-cyclers-time-" + fmt.Sprintf("%d", timeLimit) + "-space-" + fmt.Sprintf("%d", spaceLimit) + "-minIndex-" + fmt.Sprintf("%d", minIndex) + "-maxIndex-" + fmt.Sprintf("%d", maxIndex)
+	f, _ := os.OpenFile(runName,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	var wg sync.WaitGroup
+
+	startTime := time.Now()
+
+	for i := 0; i < nWorkers; i += 1 {
+		wg.Add(1)
+		go func(iWorker int, nWorkers int) {
+			k := 0
+			for n := minIndex + iWorker; n < maxIndex; n += nWorkers {
+				if k%1000 == 0 {
+					fmt.Println(time.Since(startTime), "Worker: ", iWorker, "k: ", k)
+				}
+				m, err := bbc.GetMachineI(DB[:], n, true)
+				if err != nil {
+					fmt.Println("Err:", err, n)
+				}
+				if argumentTranslatedCyclers(m, timeLimit, spaceLimit) {
+					var arr [4]byte
+					binary.BigEndian.PutUint32(arr[0:4], uint32(n))
+					f.Write(arr[:])
+				}
+				k += 1
+			}
+			wg.Done()
+		}(i, nWorkers)
+	}
+
+	wg.Wait()
+	f.Close()
 }
---
+++ argument-translated-cyclers/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	bbc "github.com/bbchallenge/bbchallenge-go"
+)
+
+func TestArgumentTranslatedCyclers(t *testing.T) {
+	DB, err := ioutil.ReadFile(DB_PATH)
+	if err != nil {
+		t.Fail()
+	}
+	// Non-cyclers machines (halting machines and divergent ones)
+	t.Run("argument_translated-cyclers_bb5", func(t *testing.T) {
+		if argumentTranslatedCyclers(bbc.GetBB5Winner(), 1000, 500) {
+			t.Fail()
+		}
+	})
+
+	divergent_indices := []int{7888060, 5351679, 7199289, 7177945, 12930717, 16322779, 41540523, 8852034, 14203995, 294145, 13128060, 12023841, 3666257, 56021278}
+
+	for i := range divergent_indices {
+		index := divergent_indices[i]
+		t.Run(fmt.Sprintf("argument_translated-cyclers_divergent_%d", index), func(t *testing.T) {
+			tm, err := bbc.GetMachineI(DB[:], index, true)
+			if err != nil {
+				t.Fail()
+			}
+			if argumentTranslatedCyclers(tm, 1000, 500) {
+				t.Fail()
+			}
+		})
+	}
+
+	translated_cyclers_indices := []int{78619822, 52297459, 37549149, 37799884, 33613794, 65712201, 73643020, 73823886, 87711504}
+
+	for i := range translated_cyclers_indices {
+		index := translated_cyclers_indices[i]
+		t.Run(fmt.Sprintf("argument_translated-cyclers_translated-cyclers_%d", index), func(t *testing.T) {
+			tm, err := bbc.GetMachineI(DB[:], index, true)
+			if err != nil {
+				t.Fail()
+			}
+			if !argumentTranslatedCyclers(tm, 1000, 500) {
+				t.Fail()
+			}
+		})
+	}
+
+}
