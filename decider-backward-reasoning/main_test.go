4799d82867b944a8113547a17e92aef51b6be076
Cosmo Stérin <tristan.sterin@mu.ie>
2022-03-27 20:47:42 +0100

    Update main_test.go

--- decider-backward-reasoning/main_test.go
+++ decider-backward-reasoning/main_test.go
@@ -14,7 +14,9 @@ func TestArgumentBackwardReasoning(t *testing.T) {
 		t.Fail()
 	}
 
-	// Obsolete indices since DB sorting operation 6/03/22 not_backward_reasoning_indices := []int{40851850, 11710205, 11726152, 72903966, 10039500, 4966400, 13754164}
+	// Obsolete indices since DB sorting operation 6/03/22 
+	// not_backward_reasoning_indices := []int{40851850, 11710205, 11726152, 72903966, 10039500, 4966400, 13754164}
+	// Below are new indices
 	not_backward_reasoning_indices := []int{73261028, 8527536, 7911681, 28086713, 11059089, 11670429, 7865218}
 	for i := range not_backward_reasoning_indices {
 		index := not_backward_reasoning_indices[i]
@@ -29,8 +31,9 @@ func TestArgumentBackwardReasoning(t *testing.T) {
 		})
 	}
 
-	// Obsolete indices since DB sorting operation 6/03/22 backward_reasoning_indices := []int{13955979, 54221304, 7850055, 7658575, 7274055, 620647, 392407}
-
+	// Obsolete indices since DB sorting operation 6/03/22 
+	// backward_reasoning_indices := []int{13955979, 54221304, 7850055, 7658575, 7274055, 620647, 392407}
+	// Below are new indices
 	backward_reasoning_indices := []int{4843748, 58360621, 2009846, 1973992, 11176971, 4147941, 12071224}
 
 	for i := range backward_reasoning_indices {
