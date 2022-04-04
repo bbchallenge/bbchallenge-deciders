911365b81553a7318a9dafe0294b3e19d356bc71
modderme123 <modderme123@gmail.com>
2022-04-03 21:32:57 -0700

    expect machines to only have one undefined transition

--- decider-description_full/description_full.c
+++ decider-description_full/description_full.c
@@ -10,15 +10,14 @@ int main() {
 
   for (int i = 0; i < 88664064; i += 1) {
     fread(buffer, 30, 1, f);
-    int description_full = 1;
+    int undefined_transitions = 0;
     for (int j = 0; j < 10; j += 1) {
       if (buffer[3 * j] == 0) {
-        description_full = 0;
-        break;
+        undefined_transitions += 1;
       }
     }
 
-    if (description_full) {
+    if (undefined_transitions == 0 || undefined_transitions > 1) {
       printf("%d\n", i);
     }
   }
