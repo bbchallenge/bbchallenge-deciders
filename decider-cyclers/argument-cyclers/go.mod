6a27ce43adab8b94da58a42359e9240337ebf907
Tristan Stérin <tristan.sterin@mu.ie>
2022-05-12 21:20:03 +0100

    cyclers and make undecideded index

--- decider-cyclers/argument-cyclers/go.mod
+++
@@ -1,8 +0,0 @@
-module bbchallenge-arguments/argument-cyclers
-
-go 1.16
-
-require (
-	github.com/bbchallenge/bbchallenge-go v0.0.0-20220129112458-2f402ee4f4d4
-	golang.org/x/exp v0.0.0-20220128181451-c853b6ddb95e // indirect
-)
--- make-undecided-index/main.go
+++ make-undecided-index/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -10,36 +11,50 @@ import (
 )
 
 const DECIDED_INDEXES = "../bb5_decided_indexes/"
+const HEURISTIC_INDEXES = "../bb5_heuristically_decided_indexes/"
 
 func main() {
+
+	argHeuristic := flag.Bool("h", false, "apply heuristics as well as decider to get final index")
+
+	flag.Parse()
+	applyHeuristc := *argHeuristic
 
 	var decidedIndexes map[uint32]string = make(map[uint32]string)
 	var undecidedIndexByte []byte
 
-	items, _ := ioutil.ReadDir(DECIDED_INDEXES)
-	for _, item := range items {
-		if item.IsDir() || item.Name()[0] == '.' {
-			continue
-		} else {
+	indexes_to_consider := []string{DECIDED_INDEXES}
 
-			fmt.Println("Reading", item.Name())
-			index, err := ioutil.ReadFile(DECIDED_INDEXES + item.Name())
-			fmt.Println(len(index)/4, "decided machines")
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(-1)
-			}
+	if applyHeuristc {
+		indexes_to_consider = append(indexes_to_consider, HEURISTIC_INDEXES)
+	}
 
-			for i := 0; i < len(index); i += 4 {
-				machineIndex := binary.BigEndian.Uint32(index[i : i+4])
-				if _, ok := decidedIndexes[machineIndex]; ok {
-					fmt.Println("Machine", machineIndex, "already there from", decidedIndexes[machineIndex])
+	for i_case, index_folder := range indexes_to_consider {
+		items, _ := ioutil.ReadDir(index_folder)
+		for _, item := range items {
+			if item.IsDir() || item.Name()[0] == '.' {
+				continue
+			} else {
+
+				fmt.Println("Reading", item.Name())
+				index, err := ioutil.ReadFile(index_folder + item.Name())
+				fmt.Println(len(index)/4, "decided machines")
+				if err != nil {
+					fmt.Println(err)
 					os.Exit(-1)
 				}
-				decidedIndexes[machineIndex] = item.Name()
-			}
 
-			fmt.Println("Done with", item.Name(), "\n")
+				for i := 0; i < len(index); i += 4 {
+					machineIndex := binary.BigEndian.Uint32(index[i : i+4])
+					if _, ok := decidedIndexes[machineIndex]; ok && i_case == 0 {
+						fmt.Println("Machine", machineIndex, "already there from", decidedIndexes[machineIndex])
+						os.Exit(-1)
+					}
+					decidedIndexes[machineIndex] = item.Name()
+				}
+
+				fmt.Println("Done with", item.Name(), "\n")
+			}
 		}
 	}
 
@@ -55,10 +70,17 @@ func main() {
 		}
 	}
 
+	outputFile := "output/bb5_undecided_index"
+	if applyHeuristc {
+		outputFile = "output/bb5_undecided_index_with_heuristics"
+	}
 	fmt.Println("There are", len(undecidedIndexByte)/4, "undecided machines our of", bbc.TOTAL_UNDECIDED)
-	err := os.WriteFile("output/bb5_undecided_index", undecidedIndexByte, 0644)
+
+	err := os.WriteFile(outputFile, undecidedIndexByte, 0644)
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+
 }
